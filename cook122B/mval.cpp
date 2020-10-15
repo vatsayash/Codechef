20010e25bbe11d93875600847baaaf06bbc12c34
= <vatsayash@gmail.com>
2020-10-16 00:38:45 +0530

    cook122B added

---
+++ cook122B/mval.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+using namespace std;
+#define vi vector<int>
+#define vvi vector<vector<int> > 
+#define vl vector<long long>
+#define vc vector<char> 
+#define vvc vector<vector<char> > 
+#define vpi vector<pair<int,int> >
+#define ll long long 
+#define sz(a) int(a.size())
+#define forn(i,n) for(int i = 0 ; i < int(n) ; i++)
+#define all(x) x.begin(),x.end()
+#define vpl vector<pair<long long,long long> > 
+#define F first
+#define S second
+#define mp make_pair
+#define ld long double
+#define pb push_back
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pci pair<char,int>
+#define vpii vector<pii> 
+#define vpll vector<pll> 
+#define len(s) s.length()
+#define un unsigned
+# define M_PI           3.14159265358979323846  /* pi */
+#define modulo 998244353ll
+#define mod 1000000007ll
+#define MAXN  1000001 
+#define fast() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll power(ll a,ll b){return b?power(a*a%mod,b/2)*(b%2?a:1)%mod:1;}
+ll gcd(ll a,ll b){if(a==0 || b == 0) return max(a,b);if(a == 1 || b == 1)return 1;return gcd(max(a,b)%min(a,b),min(a,b));}
+
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vl a(n,0);
+    forn(i,n)
+    cin>>a[i];
+    ll ans = 0;
+    ll ss = 0;
+    int i = 0;
+    int j = n-1;
+    vi subs;
+    while(i < j)
+    {
+        while(a[i] >= 0 && i < n)
+        i++;
+        while(a[j] < 0 && j >= 0)
+        j--;
+        if(i < j)
+        {
+            swap(a[i],a[j]);
+            subs.pb(i+1);
+            subs.pb(j+1);
+        }
+    }
+    sort(all(subs));
+    for(auto u : a )
+    ss += max(0ll,u);
+    ans = max(ans,ss);
+    cout<<ans<<endl;
+    cout<<subs.size()<<" ";
+    for(auto u : subs)
+    cout<<u<<" ";
+    cout<<endl;
+}
+
+int main()    
+{
+    fast();
+    int t;
+    t = 1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
+
